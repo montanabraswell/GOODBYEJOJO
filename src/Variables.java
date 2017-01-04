abcf228158b6b4aa957af1194575ebd7206b8e37
montanabraswell <montanabraswell1@gmail.com>
2017-01-03 19:06:51 -0500

    Second Commit

---
+++ src/Variables.java
@@ -0,0 +1,30 @@
+/**Java Tutorial 2
+ * using variables and initializing them
+ * also using a print statement and identifying println and print
+ * @author Montana
+ */
+
+public class Variables {
+	public static void main(String args[]){
+	 	
+		// creating a variable
+		double goodbyeJoJo;
+		
+		//initializing the variable with a value
+	 	goodbyeJoJo =999;
+	 	
+	 	//printing out the statements on one line
+	 	System.out.print(" IT WAS I! DIO!\t");
+	 	System.out.print(goodbyeJoJo);
+	 	System.out.println("\tWRRRRYYYYYYYYYY");
+	 	
+	 	//printing using println so it goes on the next line
+	 	System.out.println("\tinsert Joseph Joestar");
+	
+
+}
+
+	 	
+	 	
+	 	
+}
